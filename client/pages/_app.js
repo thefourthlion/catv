dac6cb24ac37347f4c4291dc26ae616a3d12268b
TheFourthLion <the00110100lion@gmail.com>
2023-04-25 07:00:58 -0700

    add dropdown for freq and cable size

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,7 @@
 import "../styles/globals.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/index.scss";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,91 @@
+import { useState } from "react";
+
+import { Dropdown } from "react-bootstrap";
 export default function Home() {
-  return <div className="Home"></div>;
+  const [selectedCable, setSelectedCable] = useState("Select Coax Size");
+  const [selectedFrequency, setSelectedFrequency] =
+    useState("Select Frequency");
+  const handleCableSizeChoice = (option) => {
+    setSelectedCable(option);
+  };
+
+  const handleFrequencyChoice = (option) => {
+    setSelectedCable(option);
+  };
+
+  return (
+    <div className="Home">
+      <div className="cable-loss-container">
+        <div className="cable-size-dropdown">
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="secondary"
+              id="dropdown-basic"
+              className="dropdown"
+            >
+              {selectedCable}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item
+                className="dropdown-item"
+                onClick={() => handleCableSizeChoice("RG59")}
+              >
+                Coax - RG59
+              </Dropdown.Item>
+
+              <Dropdown.Item
+                className="dropdown-item"
+                onClick={() => handleCableSizeChoice("RG6")}
+              >
+                Coax - RG6
+              </Dropdown.Item>
+
+              <Dropdown.Item
+                className="dropdown-item"
+                onClick={() => handleCableSizeChoice("RG11")}
+              >
+                Coax - RG11
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+
+        <div className="frequency-dropdown">
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="secondary"
+              id="dropdown-basic"
+              className="dropdown"
+            >
+              {selectedFrequency}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item
+                className="dropdown-item"
+                onClick={() => handleFrequencyChoice("36MHz")}
+              >
+                CH3 - 36 MHz
+              </Dropdown.Item>
+
+              <Dropdown.Item
+                className="dropdown-item"
+                onClick={() => handleFrequencyChoice("297MHz")}
+              >
+                CH36 - 297 MHz
+              </Dropdown.Item>
+
+              <Dropdown.Item
+                className="dropdown-item"
+                onClick={() => handleFrequencyChoice("501MHz")}
+              >
+                CH70 - 501 MHz
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </div>
+    </div>
+  );
 }
