ba37cbed3a7997d357a000ee02f231e45f42f88b
TheFourthLion <the00110100lion@gmail.com>
2023-04-25 18:42:06 -0700

    added functionality to connectors

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,6 +10,17 @@ export default function Home() {
   const [measurement, setMeasurement] = useState("Cable Ft");
   const [showConnectors, setShowConnectors] = useState(false);
 
+  const [barrelCount, setBarrelCount] = useState(0);
+  const [twoWayCount, setTwoWayCount] = useState(0);
+  const [threeWayCount, setThreeWayCount] = useState(0);
+  const [fourWayCount, setFourWayCount] = useState(0);
+  const [fiveWayCount, setFiveWayCount] = useState(0);
+  const [sixWayCount, setSixWayCount] = useState(0);
+  const [eightWayCount, setEightWayCount] = useState(0);
+  const [twelveWayCount, setTwelveWayCount] = useState(0);
+
+  const [attenuation, setAttenuation] = useState(0);
+
   const handleCableSizeChoice = (option) => {
     setSelectedCable(option);
   };
@@ -134,23 +145,192 @@ export default function Home() {
         {showConnectors && (
           <div className="connectors">
             <div className="square-btns">
-              <button className="square-btn">Barrel</button>
-              <button className="square-btn">2-Way</button>
+              <button
+                className="square-btn"
+                onClick={() => {
+                  setBarrelCount(barrelCount + 1);
+                  setAttenuation(attenuation - 0.5);
+                }}
+              >
+                Barrel
+              </button>
+              <button
+                onClick={() => {
+                  setTwoWayCount(twoWayCount + 1);
+                  setAttenuation(attenuation - 3.5);
+                }}
+                className="square-btn"
+              >
+                2-Way
+              </button>
             </div>
+
+            {barrelCount || twoWayCount ? (
+              <div className="square-btns">
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setBarrelCount(barrelCount - 1);
+                    setAttenuation(attenuation + 0.5);
+                  }}
+                >
+                  {barrelCount}
+                </h2>
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setTwoWayCount(twoWayCount - 1);
+                    setAttenuation(attenuation + 3.5);
+                  }}
+                >
+                  {twoWayCount}
+                </h2>
+              </div>
+            ) : (
+              ""
+            )}
             <div className="square-btns">
-              <button className="square-btn">3-Way</button>
-              <button className="square-btn">4-Way</button>
+              <button
+                className="square-btn"
+                onClick={() => {
+                  setThreeWayCount(threeWayCount + 1);
+                  setAttenuation(attenuation - 7);
+                }}
+              >
+                3-Way
+              </button>
+              <button
+                className="square-btn"
+                onClick={() => {
+                  setFourWayCount(fourWayCount + 1);
+                  setAttenuation(attenuation - 3.5);
+                }}
+              >
+                4-Way
+              </button>
             </div>
+
+            {threeWayCount || fourWayCount ? (
+              <div className="square-btns">
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setThreeWayCount(threeWayCount - 1);
+                    setAttenuation(attenuation + 0.5);
+                  }}
+                >
+                  {threeWayCount}
+                </h2>
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setFourWayCount(fourWayCount - 1);
+                    setAttenuation(attenuation + 3.5);
+                  }}
+                >
+                  {fourWayCount}
+                </h2>
+              </div>
+            ) : (
+              ""
+            )}
+
             <div className="square-btns">
-              <button className="square-btn">5-Way</button>
-              <button className="square-btn">6-Way</button>
+              <button
+                className="square-btn"
+                onClick={() => {
+                  setFiveWayCount(fiveWayCount + 1);
+                  setAttenuation(attenuation - 3.5);
+                }}
+              >
+                5-Way
+              </button>
+              <button
+                className="square-btn"
+                onClick={() => {
+                  setSixWayCount(sixWayCount + 1);
+                  setAttenuation(attenuation - 3.5);
+                }}
+              >
+                6-Way
+              </button>
             </div>
+
+            {fiveWayCount || sixWayCount ? (
+              <div className="square-btns">
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setFiveWayCount(fiveWayCount - 1);
+                    setAttenuation(attenuation + 0.5);
+                  }}
+                >
+                  {fiveWayCount}
+                </h2>
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setSixWayCount(sixWayCount - 1);
+                    setAttenuation(attenuation + 3.5);
+                  }}
+                >
+                  {sixWayCount}
+                </h2>
+              </div>
+            ) : (
+              ""
+            )}
+
             <div className="square-btns">
-              <button className="square-btn">8-Way</button>
-              <button className="square-btn">12-Way</button>
+              <button
+                className="square-btn"
+                onClick={() => {
+                  setEightWayCount(eightWayCount + 1);
+                  setAttenuation(attenuation - 3.5);
+                }}
+              >
+                8-Way
+              </button>
+              <button
+                className="square-btn"
+                onClick={() => {
+                  setTwelveWayCount(twelveWayCount + 1);
+                  setAttenuation(attenuation - 3.5);
+                }}
+              >
+                12-Way
+              </button>
             </div>
+
+            {eightWayCount || twelveWayCount ? (
+              <div className="square-btns">
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setEightWayCount(eightWayCount - 1);
+                    setAttenuation(attenuation + 0.5);
+                  }}
+                >
+                  {eightWayCount}
+                </h2>
+
+                <h2
+                  className="count"
+                  onClick={() => {
+                    setTwelveWayCount(twelveWayCount - 1);
+                    setAttenuation(attenuation + 3.5);
+                  }}
+                >
+                  {twelveWayCount}
+                </h2>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         )}
+
+        <h1>attenuation {attenuation}</h1>
       </div>
     </div>
   );
